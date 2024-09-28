e95c14fbcc5811e21d60ef2d9c22dae5c70e8eb9
Zaahir Dawood <zaahirdawood@gmail.com>
2024-09-28 13:06:48 +0100

    CI/CD pipeline output and conclusions

--- models/tpot_pipeline.py
+++ models/tpot_pipeline.py
@@ -1,11 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
+from sklearn.svm import LinearSVC
 from tpot.builtins import StackingEstimator
-from sklearn.preprocessing import FunctionTransformer
-from copy import copy
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -13,13 +12,10 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.8690238136340042
+# Average CV score on the training set was: 0.8689704626945703
 exported_pipeline = make_pipeline(
-    make_union(
-        FunctionTransformer(copy),
-        FunctionTransformer(copy)
-    ),
-    ExtraTreesClassifier(bootstrap=False, criterion="entropy", max_features=0.1, min_samples_leaf=1, min_samples_split=12, n_estimators=100)
+    StackingEstimator(estimator=LinearSVC(C=25.0, dual=True, loss="hinge", penalty="l2", tol=0.1)),
+    RandomForestClassifier(bootstrap=False, criterion="entropy", max_features=0.5, min_samples_leaf=2, min_samples_split=10, n_estimators=100)
 )
 
 exported_pipeline.fit(training_features, training_target)
